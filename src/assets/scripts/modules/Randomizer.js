605d088ea9325805ffda927eec8101351e7ff543
Sunny <ousunny91@gmail.com>
2018-12-06 16:23:47 -0500

    Complete Randomizer

--- src/assets/scripts/modules/Randomizer.js
+++ src/assets/scripts/modules/Randomizer.js
@@ -3,26 +3,55 @@ import $ from 'jquery';
 class Randomizer {
   constructor() {
     this.list = [];
+    this.randomizerLength = 3;
+
+    this.randomizerText = $('.js-randomizer__text');
+
     this.input = $('.js-input-container__input');
-    this.btn = $('.js-input-container__btn');
+    this.addBtn = $('.js-input-container__btn');
+
+    this.randomizerBtn = $('.js-randomizer-btn');
     this.events();
   }
 
   events() {
-    this.btn.click(this.addToList.bind(this));
+    this.input.keyup((e) => {
+      if (e.keyCode === 13) {
+        this.addToList();
+      }
+    });
+    this.addBtn.click(this.addToList.bind(this));
+    this.randomizerBtn.click(this.start.bind(this));
   }
 
   addToList() {
     var trimmedInput = $.trim(this.input.val());
     if (trimmedInput.length > 0) {
       this.list.push(trimmedInput);
-      console.log('Added to list');
-    } else {
-      console.log('Not valid input')
     }
 
     this.input.val('');
-    console.log(this.list);
+  }
+
+  start() {
+    if (this.list.length <= 0) {
+      return;
+    }
+
+    var counter = 0;
+
+    var timer = setInterval(() => {
+      if (counter++ >= this.randomizerLength * 20) {
+        clearInterval(timer);
+        return;
+      }
+      this.randomize();
+    }, 50);
+  }
+
+  randomize() {
+    var randomItem = this.list[Math.floor(Math.random() * this.list.length)];
+    this.randomizerText.text(randomItem.toString());
   }
 }
 
