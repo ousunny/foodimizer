ff4e117d87d17dc16fbd728113800b3e790669e1
Sunny <ousunny91@gmail.com>
2018-12-06 17:36:10 -0500

    Add error styling and animations to buttons

--- src/assets/scripts/modules/Randomizer.js
+++ src/assets/scripts/modules/Randomizer.js
@@ -20,14 +20,20 @@ class Randomizer {
         this.addToList();
       }
     });
+
     this.addBtn.click(this.addToList.bind(this));
+    this.addBtn.on('animationend', this.removeErrorClasses.bind(this));
+
     this.randomizerBtn.click(this.start.bind(this));
+    this.randomizerBtn.on('animationend', this.removeErrorClasses.bind(this));
   }
 
   addToList() {
     var trimmedInput = $.trim(this.input.val());
     if (trimmedInput.length > 0) {
       this.list.push(trimmedInput);
+    } else {
+      this.addBtn.addClass('input-container__btn--rotate-shake btn--red-text');
     }
 
     this.input.val('');
@@ -35,14 +41,21 @@ class Randomizer {
 
   start() {
     if (this.list.length <= 0) {
+      this.randomizerBtn.addClass('btn--shake btn--bg-red')
       return;
     }
 
     var counter = 0;
 
     var timer = setInterval(() => {
+      this.randomizerBtn.prop('disabled', true).addClass('btn--disabled');
+      this.input.prop('disabled', true);
+      this.addBtn.addClass('input-container__btn--disabled');
       if (counter++ >= this.randomizerLength * 20) {
         clearInterval(timer);
+        this.randomizerBtn.prop('disabled', false).removeClass('btn--disabled');
+        this.input.prop('disabled', false);
+        this.addBtn.removeClass('input-container__btn--disabled');
         return;
       }
       this.randomize();
@@ -52,6 +65,11 @@ class Randomizer {
   randomize() {
     var randomItem = this.list[Math.floor(Math.random() * this.list.length)];
     this.randomizerText.text(randomItem.toString());
+  }
+
+  removeErrorClasses() {
+    this.addBtn.removeClass('input-container__btn--rotate-shake btn--red-text');
+    this.randomizerBtn.removeClass('btn--shake btn--bg-red');
   }
 }
 
